62e914f96910ed10f958276e235ed76e031d754e
dalaba7046 <a27936343@gmail.com>
2024-06-08 10:57:13 +0800

    new code

---
+++ c.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(){
+    int n,m,a,b;
+    scanf("%d %d",&n,&m);
+
+    int input[n][m];
+    int visit[n];
+
+    for (int i=0;i<n;i++){
+        visit[i] = i;
+    }
+    for(int i=0;i<a;++i){
+        for(int j=0;j<b;++j){
+            scanf("%d %d",&a,&b);
+        }
+    }
+
+    // 如果景點[i]沒有走過,判斷是不是=1
+    if (visit[i] == i){
+
+    }
+        // 如果走過景點[i] ,將1改成0
+        visit[i] = 0
+
+}
